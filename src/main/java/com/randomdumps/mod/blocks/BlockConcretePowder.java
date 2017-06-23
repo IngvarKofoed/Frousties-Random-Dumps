784f81e7c0312f69522e4d17d3f249e09c35184b
Frousties <frost.light@hotmail.fr>
2017-06-23 13:30:50 +0200

    Updated Concrete Powder Block

--- src/main/java/com/randomdumps/mod/blocks/BlockConcretePowder.java
+++ src/main/java/com/randomdumps/mod/blocks/BlockConcretePowder.java
@@ -26,51 +26,11 @@ import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
 
 public class BlockConcretePowder extends BlockColored {
-	
-	public static final PropertyEnum<EnumDyeColor> COLOR = PropertyEnum.<EnumDyeColor>create("color", EnumDyeColor.class);
-	
 	public BlockConcretePowder() {
 		super(Material.SAND);
 		setUnlocalizedName(Reference.DumpBlocks.CONCRETE_POWDER.getUnlocalizedName());
 		setRegistryName(Reference.DumpBlocks.CONCRETE_POWDER.getRegistryName());
 		setCreativeTab(RandomDumps.RANDOM_TAB);
 		setSoundType(blockSoundType.GROUND);
-		
-		this.setDefaultState(this.blockState.getBaseState().withProperty(COLOR, EnumDyeColor.WHITE));
 	}
-	
-	public MapColor getMapColor(IBlockState state)
-    {
-        return ((EnumDyeColor)state.getValue(COLOR)).getMapColor();
-    }
-	
-	@Override
-    public int getMetaFromState(IBlockState state) {
-        return state.getValue(COLOR).getDyeDamage();
-    }
-   
-    @Override
-    public IBlockState getStateFromMeta(int meta) {
-        return getDefaultState().withProperty(COLOR, EnumDyeColor.byDyeDamage(meta));
-    }
-   
-    @Override
-    protected BlockStateContainer createBlockState() {
-        return new BlockStateContainer(this, COLOR);
-    }
-   
-    @Override
-    public boolean isFullCube(IBlockState state) {
-        return false;
-    }
-   
-    @Override
-    public boolean isOpaqueCube(IBlockState state) {
-        return false;
-    }
-   
-    @Override
-    public BlockRenderLayer getBlockLayer() {
-        return BlockRenderLayer.SOLID;
-    }
 }
