44d24108169877e25ee6a7b86734d6c698fe224f
Frousties <frost.light@hotmail.fr>
2017-06-23 20:02:35 +0200

    Almost Finished Concrete Powder Block

--- src/main/java/com/randomdumps/mod/init/RandomItems.java
+++ src/main/java/com/randomdumps/mod/init/RandomItems.java
@@ -1,5 +1,6 @@
 package com.randomdumps.mod.init;
 
+import com.randomdumps.mod.blocks.BlockConcretePowder;
 import com.randomdumps.mod.items.ItemBlockConcrete;
 import com.randomdumps.mod.items.ItemCakeBits;
 import com.randomdumps.mod.items.ItemFancyStick;
@@ -10,6 +11,7 @@ import com.randomdumps.mod.items.ItemRecordTest;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
+import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class RandomItems extends Item {
@@ -37,6 +39,39 @@ public class RandomItems extends Item {
 		GameRegistry.register(itemCakeBits);
 		GameRegistry.register(itemRecordTest);
 		GameRegistry.register(itemBlockConcrete);
+		
+		ModelResourceLocation itemModelResourceLocation = new ModelResourceLocation("randomdumps:BlockConcretePowder", "inventory");
+	    ModelLoader.setCustomModelResourceLocation(RandomItems.itemBlockConcrete, BlockConcretePowder.EnumColor.WHITE.getMetadata(), itemModelResourceLocation);
+	    itemModelResourceLocation = new ModelResourceLocation("randomdumps:BlockConcretePowderOrange", "inventory");
+	    ModelLoader.setCustomModelResourceLocation(RandomItems.itemBlockConcrete, BlockConcretePowder.EnumColor.ORANGE.getMetadata(), itemModelResourceLocation);
+	    itemModelResourceLocation = new ModelResourceLocation("randomdumps:BlockConcretePowderMagenta", "inventory");
+	    ModelLoader.setCustomModelResourceLocation(RandomItems.itemBlockConcrete, BlockConcretePowder.EnumColor.MAGENTA.getMetadata(), itemModelResourceLocation);
+	    itemModelResourceLocation = new ModelResourceLocation("randomdumps:BlockConcretePowderLightBlue", "inventory");
+	    ModelLoader.setCustomModelResourceLocation(RandomItems.itemBlockConcrete, BlockConcretePowder.EnumColor.LIGHT_BLUE.getMetadata(), itemModelResourceLocation);
+	    itemModelResourceLocation = new ModelResourceLocation("randomdumps:BlockConcretePowderYellow", "inventory");
+	    ModelLoader.setCustomModelResourceLocation(RandomItems.itemBlockConcrete, BlockConcretePowder.EnumColor.YELLOW.getMetadata(), itemModelResourceLocation);
+	    itemModelResourceLocation = new ModelResourceLocation("randomdumps:BlockConcretePowderLime", "inventory");
+	    ModelLoader.setCustomModelResourceLocation(RandomItems.itemBlockConcrete, BlockConcretePowder.EnumColor.LIME.getMetadata(), itemModelResourceLocation);
+	    itemModelResourceLocation = new ModelResourceLocation("randomdumps:BlockConcretePowderPink", "inventory");
+	    ModelLoader.setCustomModelResourceLocation(RandomItems.itemBlockConcrete, BlockConcretePowder.EnumColor.PINK.getMetadata(), itemModelResourceLocation);
+	    itemModelResourceLocation = new ModelResourceLocation("randomdumps:BlockConcretePowderGray", "inventory");
+	    ModelLoader.setCustomModelResourceLocation(RandomItems.itemBlockConcrete, BlockConcretePowder.EnumColor.GRAY.getMetadata(), itemModelResourceLocation);
+	    itemModelResourceLocation = new ModelResourceLocation("randomdumps:BlockConcretePowderSilver", "inventory");
+	    ModelLoader.setCustomModelResourceLocation(RandomItems.itemBlockConcrete, BlockConcretePowder.EnumColor.SILVER.getMetadata(), itemModelResourceLocation);
+	    itemModelResourceLocation = new ModelResourceLocation("randomdumps:BlockConcretePowderCyan", "inventory");
+	    ModelLoader.setCustomModelResourceLocation(RandomItems.itemBlockConcrete, BlockConcretePowder.EnumColor.CYAN.getMetadata(), itemModelResourceLocation);
+	    itemModelResourceLocation = new ModelResourceLocation("randomdumps:BlockConcretePowderPurple", "inventory");
+	    ModelLoader.setCustomModelResourceLocation(RandomItems.itemBlockConcrete, BlockConcretePowder.EnumColor.PURPLE.getMetadata(), itemModelResourceLocation);
+	    itemModelResourceLocation = new ModelResourceLocation("randomdumps:BlockConcretePowderBlue", "inventory");
+	    ModelLoader.setCustomModelResourceLocation(RandomItems.itemBlockConcrete, BlockConcretePowder.EnumColor.BLUE.getMetadata(), itemModelResourceLocation);
+	    itemModelResourceLocation = new ModelResourceLocation("randomdumps:BlockConcretePowderBrown", "inventory");
+	    ModelLoader.setCustomModelResourceLocation(RandomItems.itemBlockConcrete, BlockConcretePowder.EnumColor.BROWN.getMetadata(), itemModelResourceLocation);
+	    itemModelResourceLocation = new ModelResourceLocation("randomdumps:BlockConcretePowderGreen", "inventory");
+	    ModelLoader.setCustomModelResourceLocation(RandomItems.itemBlockConcrete, BlockConcretePowder.EnumColor.GREEN.getMetadata(), itemModelResourceLocation);
+	    itemModelResourceLocation = new ModelResourceLocation("randomdumps:BlockConcretePowderRed", "inventory");
+	    ModelLoader.setCustomModelResourceLocation(RandomItems.itemBlockConcrete, BlockConcretePowder.EnumColor.RED.getMetadata(), itemModelResourceLocation);
+	    itemModelResourceLocation = new ModelResourceLocation("randomdumps:BlockConcretePowderBlack", "inventory");
+	    ModelLoader.setCustomModelResourceLocation(RandomItems.itemBlockConcrete, BlockConcretePowder.EnumColor.BLACK.getMetadata(), itemModelResourceLocation);
 	}
 	
 	public static void registerRenders() {
